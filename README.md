09c37584a8ad899567526973f0928ae852c989ae
davodbeheshti <davood.beheshti2018@gmail.com>
2022-02-04 23:43:21 +0330

    a minor changes in all projects

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AppTestDavod
+# treeTablePrimeNG
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.2.1.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 
+import {TreeTableModule} from 'primeng/treetable';
+
+
+const importPackagesPrimeNG = [
+  TreeTableModule
+]
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    SharedModule
+    SharedModule,
+    ...importPackagesPrimeNG
+  ],
+  exports : [
+    ...importPackagesPrimeNG
   ],
   providers: [],
   bootstrap: [AppComponent]
