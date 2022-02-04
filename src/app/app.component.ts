b21c9199cd0da1f78091e320ff2ffe2fe15e5956
davodbeheshti <davood.beheshti2018@gmail.com>
2022-02-04 23:53:31 +0330

    adding package uuid and strict project change true

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,98 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as uuid from 'uuid';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'treeTablePrimeNG';
+export class AppComponent implements OnInit {
+
+  files: any;
+
+  ngOnInit(): void {
+    this.files =
+      [
+        {
+          "data": {
+            "name": "Documents",
+            "size": "75kb",
+            "type": "Folder"
+          },
+          "children": [
+            {
+              "data": {
+                "name": "Work",
+                "size": "55kb",
+                "type": "Folder"
+              },
+              "children": [
+                {
+                  "data": {
+                    "name": "Expenses.doc",
+                    "size": "30kb",
+                    "type": "Document"
+                  }
+                },
+                {
+                  "data": {
+                    "name": "Resume.doc",
+                    "size": "25kb",
+                    "type": "Resume"
+                  }
+                }
+              ]
+            },
+            {
+              "data": {
+                "name": "Home",
+                "size": "20kb",
+                "type": "Folder"
+              },
+              "children": [
+                {
+                  "data": {
+                    "name": "Invoices",
+                    "size": "20kb",
+                    "type": "Text"
+                  }
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "data": {
+            "name": "Pictures",
+            "size": "150kb",
+            "type": "Folder"
+          },
+          "children": [
+            {
+              "data": {
+                "name": "barcelona.jpg",
+                "size": "90kb",
+                "type": "Picture"
+              }
+            },
+            {
+              "data": {
+                "name": "primeui.png",
+                "size": "30kb",
+                "type": "Picture"
+              }
+            },
+            {
+              "data": {
+                "name": "optimus.jpg",
+                "size": "30kb",
+                "type": "Picture"
+              }
+            }
+          ]
+        }
+      ]
+  }
+
 }
