9c07945347762fa83fa9af0c2ede19a6c2cdf6ad
davodbeheshti <davood.beheshti2018@gmail.com>
2022-02-07 00:08:25 +0330

    create this section and apply operation in inputs

--- src/app/add-node/add-node.component.ts
+++ src/app/add-node/add-node.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 
 @Component({
   selector: 'app-add-node',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddNodeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dialogConfig: DynamicDialogConfig, private ref: DynamicDialogRef) { }
+
+  form: FormGroup;
 
   ngOnInit(): void {
+    debugger
+    this.form = new FormGroup({
+      nameCar: new FormControl('', Validators.required),
+      price: new FormControl('', Validators.required),
+      country: new FormControl('')
+    })
+    if (this.dialogConfig.data) {
+      const { nameCar, price, country } = this.dialogConfig.data
+      this.form.patchValue({
+        nameCar,
+        price,
+        country
+      })
+    }
+  }
+
+  public applyChanges = () => {
+    this.ref.close(this.form.value);
   }
 
 }
